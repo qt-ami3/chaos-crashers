350514a55311fe1f3b2f6cdf3f1a51c44a7c4496
Annie Val <aval@192.168.1.16>
2025-11-15 18:44:18 -0500

    added enemy functionality & health/damage system

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import (	
+import (
+	"math/rand"
 	"log"
+	"fmt"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -9,18 +11,32 @@ import (
 
 var ( //declvare variable for images, name *ebiten.Image.
 	background *ebiten.Image
-	player1 * ebiten.Image
-	//charecter2 * ebiten.Image
+	player1 *ebiten.Image
+	axeZombie *ebiten.Image
 	screenWidth = 750
 	screenHeight = 750
 	player1InitX = float64((screenWidth / 2) + 50)
 	player1InitY = float64((screenHeight / 2) + 50)
-	//player2InitX = (screenWidth / 2) - 50
-	//player2InitY = (screenHeight / 2) - 50
+	axeZombieInitXTemp = float64 (randFloat(1,100))
+	axeZombieInitYTemp = float64 (randFloat(1,100))
+	player1hp = 9999
 )
+
+
+func abs(f float64) float64 {
+  if f < 0 {
+    return -f
+  }
+  return f
+}
+
+func randFloat(min, max float64) float64 {
+  return min + rand.Float64()*(max-min)
+}
 
 func init() { //initialize images to variables here.
 	var err error
+	
 	background, _, err = ebitenutil.NewImageFromFile("assets/images/go.png") //name, _, etc.
 	if err != nil {
 		log.Fatal(err)
@@ -30,31 +46,65 @@ func init() { //initialize images to variables here.
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	axeZombie, _, err = ebitenutil.NewImageFromFile("assets/sprites/enemies/axeZombie/Sprite-0002.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type Game struct{}
 
-func (g *Game) Update() error {
+func (g *Game) Update() error { //game logic
 	
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) == true {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) == true { //player movement, inverted
 		player1InitX = player1InitX + 2
 	}
-
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) == true {
 		player1InitX = player1InitX - 2
 	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) == true {
+		player1InitY = player1InitY + 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) == true {
+		player1InitY = player1InitY - 2
+	}
 
+	if axeZombieInitXTemp < (player1InitX - 80){ //enemie movement
+		axeZombieInitXTemp++
+	}
+	if axeZombieInitXTemp > (player1InitX + 80){
+		axeZombieInitXTemp--
+	}
+	if axeZombieInitYTemp < (player1InitY - 80){
+		axeZombieInitYTemp++
+	}
+	if axeZombieInitYTemp > (player1InitY + 80){
+		axeZombieInitYTemp--
+	}
+
+
+	// enemy damage when close enough
+	hitRange := 100.0 // adjust to taste
+
+	if abs(axeZombieInitXTemp - player1InitX) < hitRange && abs(axeZombieInitYTemp - player1InitY) < hitRange {
+  player1hp--
+  fmt.Println("hp:", player1hp)
+}
+	
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {  //called every frame, game tick rate
+func (g *Game) Draw(screen *ebiten.Image) {  //called every frame, graphics.
 	screen.DrawImage(background, nil)
 
-	x := player1InitX
-	y := player1InitY
+	op := &ebiten.DrawImageOptions{}	
+	opAxeZombie := &ebiten.DrawImageOptions{}
 
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x,y)
+	op.GeoM.Translate(player1InitX,player1InitY)
+	opAxeZombie.GeoM.Translate(axeZombieInitXTemp,axeZombieInitYTemp)
+	
+	screen.DrawImage(axeZombie, opAxeZombie)
 
 	screen.DrawImage(player1, op)
 }
